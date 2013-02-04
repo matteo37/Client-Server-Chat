6f4fe13ff0bec0d0cbac053f3484034ad789602c
Matt Stevenson <mstevenson37@gmail.com>
2013-02-04 15:08:55 -0700

    everything ready

--- client.py
+++ client.py
@@ -1,41 +1,33 @@
 from socket import *
 import sys
 import shlex
-# reaver 
+
 # Create a TCP/IP socket
 sock = socket(AF_INET,SOCK_STREAM)
 
-# user_input = raw_input()
+# put args into variables
 user_input = sys.argv
 ip_address = user_input[1]
 port = int(float(user_input[2]))
 
-# ip_address = 'localhost'
-# port = 9020
-
-print ip_address
-print port
 # Connect the socket to the port where the server is listening
 server_address = (ip_address, port)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
-
+data = sock.recv(2000)
+print >>sys.stderr, data
 
 try:
-    
-    while(1):
-    	# Send data
+	message = ''
+	while(message.startswith('adios') == False):
+		# Send data
 		message = raw_input()
-		#print >>sys.stderr, 'sending "%s"' % message
-		sock.sendall(message)
-
-		# Look for the response
-		amount_received = 0
-		amount_expected = len(message)
-	    
-		data = sock.recv(2000)
-		print >>sys.stderr, data
-
+		if (message != ''):
+			sock.sendall(message)
+			# Look for the response
+			data = sock.recv(2000)
+			print >>sys.stderr, data
+	sock.sendall('adios')
+	quit()
+	#print >>sys.stderr, 'close'
 except:
-	print >>sys.stderr, 'closing socket'
 	sock.close()
--- server.py
+++ server.py
@@ -1,67 +1,81 @@
 from socket import *
 import sys
+from thread import *
 
 # Create a TCP/IP socket
 sock = socket(AF_INET,SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 9020)
+server_address = ('', 9020)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(1)
+sock.listen(5)
+#initialize vars
 chat_log = []
-chatname = "";
-while True:
+def new_connection(conn):
+	connection = conn
     # Wait for a connection
-	print >>sys.stderr, 'waiting for a connection'
-	connection, client_address = sock.accept()
-	
 	try:
-		print >>sys.stderr, 'connection from', client_address
-		connection.send("Welcome to Matt's Chat \n")
-        # Receive the data in small chunks and retransmit it
-		while True:
-			#print >>sys.stderr, 'start', 
-			data = connection.recv(2000)
-			processed_data = data.lower()
+		#Welcome
+		connection.send("Welcome to Matt's Chat \r\n")
+		#Loop on responses
+		data = connection.recv(2000)
+		while (data.startswith('adios') == False):
+			print data
+			processed_data = data.lower().strip()
 			return_data = ""
-			if(processed_data.startswith('adios')):
-				#connection.shutdown(socket.SHUT_RDWR)
-				connection.close()
-			elif(processed_data.startswith('help')):
-				return_data = "help lists all of the commands and their syntax \n test: <text>test data</text> echoes your test data back to you \n name: <text>your name</text> sets your username \n get lists all of the contents of the chat \n push <text>Content</text> adds a new line to the chat \n getrange # # will return the chat lines provided. \n adios quits the chat program"				
+			chatname = "";
+			if(processed_data.startswith('help')):
+				return_data = "help lists all of the commands and their syntax \n test: <text>test data</text> echoes your test data back to you \n name: <text>your name</text> sets your username \n get lists all of the contents of the chat \n push: <text>Content</text> adds a new line to the chat \n getrange # # will return the chat lines provided. \n adios quits the chat program\r\n"	
+				connection.send(return_data)			
 			elif(processed_data.startswith('test:')):
-				return_data = data[5:].strip()
+				return_data = data[5:].strip() + " \r\n"
+				connection.send(return_data)
 			elif(processed_data.startswith('name:')):
 				chatname = data[5:].strip()
-				return_data = "OK"				
+				return_data = "OK\r\n"				
+				connection.send(return_data)
 			elif(processed_data.startswith('push:')):
-				print >>sys.stderr, data[5:]
-				chat_log.append(chatname + ": " + data[5:])
-				return_data = "OK"
+				if(chatname != ""):
+					chat_log.append(chatname + ": " + data[5:])
+					return_data = "OK\r\n"
+					connection.send(return_data)
+				else:
+					connection.send("You need to give me your name first")
 			elif(processed_data.startswith('getrange')):
 				rangevals = data[8:]
 				ranges = rangevals.split()
 				if (int(ranges[0]) - 1 < 0 or int(ranges[1]) > len(chat_log)):
-					return_data = "Out of range"
+					return_data = "Out of range \r\n"
 				else:
 					for i in range(int(ranges[0]) - 1, int(ranges[1])):
-						return_data += chat_log[i] + " \n"
+						return_data += chat_log[i] + " \r\n"
+				connection.send(return_data)
 			elif(processed_data.startswith('get')):
-				return_data = "Chat Log \n"
+				return_data = "Chat Log \r\n"
 				for item in chat_log:
-					return_data += item+" \n"
+					return_data += item+" \r\n"
+				connection.send(return_data)
 			else:
 				return_data ="Invalid Command"
-			if return_data != "":
 				connection.send(return_data)
-				return_data = "";
-			else:
-				print >>sys.stderr, 'no more data from', client_address
-				break
+			data = connection.recv(2000)
 
+		if(data.startswith('adios')):
+			connection.send("OK\r\n")
+			connection.shutdown()
+			connection.close()
 	except:
         # Clean up the connection
 		connection.close()
+		#sock.close()
+try:
+	while(True):
+		print >>sys.stderr, 'waiting for a connection'
+		connection, client_address = sock.accept()
+		start_new_thread(new_connection,(connection,))
+except:
+	sock.close()		
+
