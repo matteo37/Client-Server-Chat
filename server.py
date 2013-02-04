6cf1fad149714e2c2747356caa555713e0d8827f
Matt Stevenson <mstevenson37@gmail.com>
2013-02-03 21:23:29 -0700

    made some changes

--- server.py
+++ server.py
@@ -25,25 +25,27 @@ while True:
         # Receive the data in small chunks and retransmit it
 		while True:
 			#print >>sys.stderr, 'start', 
-			data = connection.recv(16)
+			data = connection.recv(2000)
 			processed_data = data.lower()
-			print >>sys.stderr, processed_data
+			print >>sys.stderr, data
 
 			return_data = ""
 			if(processed_data.startswith('adios')):
+				#connection.shutdown(socket.SHUT_RDWR)
 				connection.close()
 			elif(processed_data.startswith('help')):
 				return_data = "\n help lists all of the commands and their syntax \n test: <text>test data</text> echoes your test data back to you \n name: <text>your name</text> sets your username \n get lists all of the contents of the chat \n push <text>Content</text> adds a new line to the chat \n getrange # # will return the chat lines provided. \n adios quits the chat program"				
 			elif(processed_data.startswith('test:')):
-				return_data = processed_data[5:].strip()
+				return_data = data[5:].strip()
 			elif(processed_data.startswith('name:')):
-				chatname = processed_data[5:].strip()
+				chatname = data[5:].strip()
 				return_data = "OK"				
 			elif(processed_data.startswith('push:')):
-				chat_log.append(chatname + ": " + processed_data[5:].strip())
+				print >>sys.stderr, data[5:]
+				chat_log.append(chatname + ": " + data[5:])
 				return_data = "OK"
 			elif(processed_data.startswith('getrange')):
-				rangevals = processed_data[8:]
+				rangevals = data[8:]
 				ranges = rangevals.split()
 				if (int(ranges[0]) - 1 < 0 or int(ranges[1]) > len(chat_log)):
 					return_data = "Out of range"
